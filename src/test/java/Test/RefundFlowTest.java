d8f075d455cfb75a18c1e2e19e3d6f5fb9d8d8f7
DRUBILAR <DRUBILAR@DRUBILAR-DEV>
2021-07-28 12:38:53 -0400

    commit

--- src/test/java/Test/RefundFlowTest.java
+++ src/test/java/Test/RefundFlowTest.java
@@ -19,7 +19,7 @@ class RefundFlowTest extends BaseTest {
 	/* Setear variable medioPago con el id del medio qeu se quiere utilizar para realizar pagos:
 	 * id     medio
 	 * 1      Servipag  
-	 * 2      Multicaja
+	 * 2      Multicaja 
 	 * 4	  Webpay1 
 	 */
 	
